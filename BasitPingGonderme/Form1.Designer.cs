ecf126bcb5b8853806a3f96279b48a34b7ea5208
Mustafa BÜKÜLMEZ <mustafabukulmez3446@gmail.com>
2019-12-29 13:24:09 +0300

    Proje Tamamlandı

---
+++ BasitPingGonderme/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BasitPingGonderme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_ping_at = new System.Windows.Forms.Button();
+            this.btn_disari_aktar = new System.Windows.Forms.Button();
+            this.txt_adres = new System.Windows.Forms.TextBox();
+            this.lst_sonuc = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_ping_at
+            // 
+            this.btn_ping_at.Location = new System.Drawing.Point(12, 21);
+            this.btn_ping_at.Name = "btn_ping_at";
+            this.btn_ping_at.Size = new System.Drawing.Size(173, 23);
+            this.btn_ping_at.TabIndex = 0;
+            this.btn_ping_at.Text = "Ping Gönder";
+            this.btn_ping_at.UseVisualStyleBackColor = true;
+            this.btn_ping_at.Click += new System.EventHandler(this.btn_ping_at_Click);
+            // 
+            // btn_disari_aktar
+            // 
+            this.btn_disari_aktar.Location = new System.Drawing.Point(12, 50);
+            this.btn_disari_aktar.Name = "btn_disari_aktar";
+            this.btn_disari_aktar.Size = new System.Drawing.Size(173, 23);
+            this.btn_disari_aktar.TabIndex = 1;
+            this.btn_disari_aktar.Text = "Dışarı Aktar";
+            this.btn_disari_aktar.UseVisualStyleBackColor = true;
+            this.btn_disari_aktar.Click += new System.EventHandler(this.btn_disari_aktar_Click);
+            // 
+            // txt_adres
+            // 
+            this.txt_adres.Location = new System.Drawing.Point(191, 23);
+            this.txt_adres.Name = "txt_adres";
+            this.txt_adres.Size = new System.Drawing.Size(321, 20);
+            this.txt_adres.TabIndex = 2;
+            this.txt_adres.Text = "www.mustafabukulmez.com";
+            // 
+            // lst_sonuc
+            // 
+            this.lst_sonuc.FormattingEnabled = true;
+            this.lst_sonuc.Location = new System.Drawing.Point(191, 50);
+            this.lst_sonuc.Name = "lst_sonuc";
+            this.lst_sonuc.Size = new System.Drawing.Size(321, 342);
+            this.lst_sonuc.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lst_sonuc);
+            this.Controls.Add(this.txt_adres);
+            this.Controls.Add(this.btn_disari_aktar);
+            this.Controls.Add(this.btn_ping_at);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ping_at;
+        private System.Windows.Forms.Button btn_disari_aktar;
+        private System.Windows.Forms.TextBox txt_adres;
+        private System.Windows.Forms.ListBox lst_sonuc;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BasitPingGonderme/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+
+namespace BasitPingGonderme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 750;
+        }
+        private Ping p = new Ping();
+        private int sayac;
+        private string durum = "";
+        private string adress = "";
+        private string zaman = "";
+        private string sonuc = "";
+
+        private void btn_disari_aktar_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\\PingLog.txt";
+            new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write).Close();
+
+            //FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            //fs.Close();
+
+            int num = 0;
+            while (true)
+            {
+                if (num >= lst_sonuc.Items.Count)
+                {
+                    Process.Start(path);
+                    return;
+                }
+                File.AppendAllText(path, lst_sonuc.Items[num].ToString());
+                num++;
+            }
+        }
+
+        private void btn_ping_at_Click(object sender, EventArgs e)
+        {
+            lst_sonuc.Items.Clear();
+            lst_sonuc.Items.Add(txt_adres.Text + " Adresine Ping işlemi Başlıyor... - " + DateTime.Now + Environment.NewLine);
+            lst_sonuc.Items.Add("--------------" + Environment.NewLine);
+            sayac = 0;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            if (sayac >= 10)
+            {
+                lst_sonuc.Items.Add("--------------" + Environment.NewLine);
+                lst_sonuc.Items.Add(sonuc + Environment.NewLine);
+                lst_sonuc.Items.Add("--------------" + Environment.NewLine);
+                timer1.Stop();
+            }
+            else
+            {
+                try
+                {
+                    PingReply reply = p.Send(txt_adres.Text);
+                    durum = reply.Status.ToString();
+                    adress = reply.Address.ToString();
+                    zaman = reply.RoundtripTime.ToString();
+                    lst_sonuc.Items.Add(string.Format("Sonuç : {0} {1} -> {2} ms.", durum, adress, zaman + Environment.NewLine));
+                    sonuc = "Ping başarı ile tamamlandı";
+                }
+                catch (PingException)
+                {
+                    lst_sonuc.Items.Add("Bilinen b\x00f6yle bir ana bilgisayar yok" + Environment.NewLine);
+                    sonuc = "Bir veya daha fazla Ping denemesi başarısız oldu";
+                    sayac = 10;
+                }
+                catch (NullReferenceException)
+                {
+                    lst_sonuc.Items.Add("Ping Atılamadı. Adres yanlış olabilir." + Environment.NewLine);
+                    sonuc = "Bir veya daha fazla Ping denemesi başarısız oldu";
+                    sayac = 10;
+                }
+            }
+        }
+    }
+}
